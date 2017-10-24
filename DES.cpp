7ab807cdf471eca9537aaba05625bf5e07093556
bjcrane <bjcrane2013@gmail.com>
2017-10-24 17:34:01 -0500

    progress on Tread test. Fixed DES.cpp

--- DES.cpp
+++ DES.cpp
@@ -158,14 +158,14 @@ vector<bool> eTablePerm(vector<bool> inputVector)
 {
   short outputVectorSize = 48;
   //E table stored as array
-  short eTable[outputVectorSize] = {32,1, 2, 3, 4, 5,
-                                    4, 5, 6, 7, 8, 9,
-                                    8, 9, 10,11,12,13,
-                                    12,13,14,15,16,17,
-                                    16,17,18,19,20,21,
-                                    20,21,22,23,24,25,
-                                    24,25,26,27,28,29,
-                                    28,29,30,31,32,1};
+  short eTable[] = {32,1, 2, 3, 4, 5,
+                    4, 5, 6, 7, 8, 9,
+                    8, 9, 10,11,12,13,
+                    12,13,14,15,16,17,
+                    16,17,18,19,20,21,
+                    20,21,22,23,24,25,
+                    24,25,26,27,28,29,
+                    28,29,30,31,32,1};
   //Vector of bools to be returned
   vector<bool> outputVector;
   //Pushes permuted values to outputVector
@@ -338,10 +338,10 @@ vector<bool> pTablePerm(vector<bool> inputVector)
 {
   short outputVectorSize = 32;
   //P table stored as vector
-  short pTable[outputVectorSize] = {16,7, 20,21,29,12,28,17,
-                                    1, 15,23,26,5, 18,31,10,
-                                    2, 8, 24,14,32,27,3, 9,
-                                    19,13,30,6, 22,11,4,25};
+  short pTable[] = {16,7, 20,21,29,12,28,17,
+                    1, 15,23,26,5, 18,31,10,
+                    2, 8, 24,14,32,27,3, 9,
+                    19,13,30,6, 22,11,4,25};
   //Vector of bools to be returned
   vector<bool> outputVector;
   //Pushes permuted values to outputVector
--- threadTest.cpp
+++ threadTest.cpp
@@ -10,44 +10,50 @@ using namespace std;
 
 struct thread_data {
    int  thread_id;
-   char *message;
+   long startingKeyNum;
 };
 
-void *PrintHello(void *threadarg) {
-   struct thread_data *my_data;
-   my_data = (struct thread_data *) threadarg;
+// void *PrintHello(void *threadarg) {
+//    struct thread_data *my_data;
+//    my_data = (struct thread_data *) threadarg;
+//
+//    cout << "Thread ID : " << my_data->thread_id << endl;
+//    cout << " Message : " << my_data->message << endl;
+//
+//    pthread_exit(NULL);
+// }
 
-   cout << "Thread ID : " << my_data->thread_id << endl;
-   cout << " Message : " << my_data->message << endl;
-
-   pthread_exit(NULL);
-}
-
-void *UseNum (void *threadArg){
-   sleep(1);
-   int *temp;
-   temp = (int *) threadArg;
-   cout << "Thread arg:" << temp << endl;
-   pthread_exit(NULL);
-}
+// void *UseNum (void *threadArg){
+//    sleep(1);
+//    int *temp;
+//    temp = (int *) threadArg;
+//    cout << "Thread arg:" << temp << endl;
+//    pthread_exit(NULL);
+// }
 
 int main () {
    pthread_t threads[NUM_THREADS];
    struct thread_data td[NUM_THREADS];
-   int rc;
-   char printMe[] = "Print me";
-   int totalKeys = pow(2, 56);
-   int numArray[5] = {128, 256, };
+   int resultCode;
+
+   long totalKeys = pow(2, 56);
+   cout << "totalKeys: " << totalKeys << endl;
+
+   for (int i = 1; i <= 58; i++) {
+      cout << (totalKeys / 58) * i << endl;
+   }
+   cout << totalKeys << endl;
+
 
    for( int i = 0; i < NUM_THREADS; i++ ) {
       cout <<"main() : creating thread, " << i << endl;
       td[i].thread_id = i;
-      td[i].message = printMe;
-      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
-      rc = pthread_create(&threads[i], NULL, UseNum, (void *)&i);
+      td[i].startingKeyNum = 5;
+      // resultCode = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
+      // resultCode = pthread_create(&threads[i], NULL, UseNum, &numArray[i]);
 
-      if (rc) {
-         cout << "Error:unable to create thread," << rc << endl;
+      if (resultCode) {
+         cout << "Error:unable to create thread," << resultCode << endl;
          exit(-1);
       }
    }
