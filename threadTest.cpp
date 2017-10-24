eda46d213e5cc0323c9483341303fc651f19a5d8
bjcrane <bjcrane2013@gmail.com>
2017-10-24 18:03:38 -0500

    Can pass threads with arguments
    Now we need to pass the right arguments

--- threadTest.cpp
+++ threadTest.cpp
@@ -13,23 +13,16 @@ struct thread_data {
    long startingKeyNum;
 };
 
-// void *PrintHello(void *threadarg) {
-//    struct thread_data *my_data;
-//    my_data = (struct thread_data *) threadarg;
-//
-//    cout << "Thread ID : " << my_data->thread_id << endl;
-//    cout << " Message : " << my_data->message << endl;
-//
-//    pthread_exit(NULL);
-// }
+void *PrintHello(void *threadarg) {
+   struct thread_data *my_data;
+   my_data = (struct thread_data *) threadarg;
 
-// void *UseNum (void *threadArg){
-//    sleep(1);
-//    int *temp;
-//    temp = (int *) threadArg;
-//    cout << "Thread arg:" << temp << endl;
-//    pthread_exit(NULL);
-// }
+   cout << "Thread ID : " << my_data->thread_id << endl;
+   cout << " Starting Key Number : " << my_data->startingKeyNum << endl;
+
+   pthread_exit(NULL);
+}
+
 
 int main () {
    pthread_t threads[NUM_THREADS];
@@ -39,18 +32,18 @@ int main () {
    long totalKeys = pow(2, 56);
    cout << "totalKeys: " << totalKeys << endl;
 
-   for (int i = 1; i <= 58; i++) {
-      cout << (totalKeys / 58) * i << endl;
+   for (int i = 1; i <= 12; i++) {
+      cout << (totalKeys / 12) * i << endl;
    }
    cout << totalKeys << endl;
 
+   int myKeyspace = totalKeys / 12;
 
    for( int i = 0; i < NUM_THREADS; i++ ) {
       cout <<"main() : creating thread, " << i << endl;
       td[i].thread_id = i;
-      td[i].startingKeyNum = 5;
-      // resultCode = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
-      // resultCode = pthread_create(&threads[i], NULL, UseNum, &numArray[i]);
+      td[i].startingKeyNum = myKeyspace * i;
+      resultCode = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
 
       if (resultCode) {
          cout << "Error:unable to create thread," << resultCode << endl;
