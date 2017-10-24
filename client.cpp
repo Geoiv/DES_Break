bc50c53d5cc622e86d2c81a97f755e49b78a85b6
Unknown <geoiv04@gmail.com>
2017-10-24 17:33:21 -0500

    Working TCP comms, only tested on localhost.

--- client.cpp
+++ client.cpp
@@ -11,39 +11,45 @@ using namespace std;
 int main()
 {
   vector<int> socketFDs;
-  int sockFileDesc;
-  string targetIP = "150.243.146.141";
+  int servFileDesc;
+  string targetIP;
+  //targetIP = "150.243.146.14";
+  targetIP = "127.0.0.1";
   struct sockaddr_in servAddress;
   memset(&servAddress, 0, sizeof(servAddress));
   servAddress.sin_family = AF_INET;
   servAddress.sin_addr.s_addr = inet_addr(targetIP.c_str());
   servAddress.sin_port =  htons(PORT_NO);
-
-  char* sendMsg = "Hello team member!";
-  char* recMsg;
+  const char* sendMsg = "Hello team member!";
+  char recMsg[MAX_LINE];
 
   for (short i = 0; i < SERVER_COUNT; i++)
   {
     //TODO protocol?
-    sockFileDesc = socket(AF_INET, SOCK_STREAM, 0);
-    socketFDs.push_back(sockFileDesc);
-    if (sockFileDesc == -1)
+    servFileDesc = socket(AF_INET, SOCK_STREAM, 0);
+    socketFDs.push_back(servFileDesc);
+    if (servFileDesc == -1)
     {
       perror("Socket descriptor creation failed.");
       exit(EXIT_FAILURE);
     }
 
     //Connection of the client to the socket
-    if (!connect(sockFileDesc, (struct sockaddr *) &servAddress,
-      sizeof(servAddress)))
+    if (connect(servFileDesc, (struct sockaddr *) &servAddress,
+      sizeof(servAddress)) == -1)
     {
       perror("Problem in connecting to the server");
       exit(EXIT_FAILURE);
     }
 
     cout << "Message sending to server: " << sendMsg << endl;
-    send(sockFileDesc, sendMsg, strlen(sendMsg), 0);
-    int receiveResult = recv(sockFileDesc, recMsg, MAX_LINE, 0);
+    int sendResult = send(servFileDesc, sendMsg, strlen(sendMsg), 0);
+    if (sendResult == -1)
+    {
+      perror("Sending message failed.");
+      exit(EXIT_FAILURE);
+    }
+    int receiveResult = recv(servFileDesc, recMsg, MAX_LINE, 0);
     if (receiveResult == 0)
     {
       cout << "No available messages. " << endl;
@@ -63,7 +69,7 @@ int main()
   bool stillLooping = false;
   while (stillLooping)
   {
-    if (recv(sockFileDesc, recMsg, MAX_LINE, 0) == 0)
+    if (recv(servFileDesc, recMsg, MAX_LINE, 0) == 0)
     {
       //error: server terminated prematurely
       perror("The server terminated prematurely");
@@ -71,6 +77,6 @@ int main()
     }
     cout << "String received from the server: " << recMsg << endl;
   }
-  close(sockFileDesc);
+  close(servFileDesc);
   return EXIT_SUCCESS;
 }
--- server.cpp
+++ server.cpp
@@ -10,35 +10,43 @@ using namespace std;
 int main()
 {
 
-  int sockFileDesc;
+  int servFileDesc;
   struct sockaddr_in servAddress;
   struct sockaddr_in clientAddress;
   socklen_t clientAddressLen;
   memset(&servAddress, 0, sizeof(servAddress));
   servAddress.sin_family = AF_INET;
-  servAddress.sin_addr.s_addr = INADDR_ANY;
+  servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
   servAddress.sin_port =  htons(PORT_NO);
-  char* sendMsg;
-  char* recMsg;
+  char recMsg[MAX_LINE];
 
   //TODO protocol?
-  sockFileDesc = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockFileDesc == -1)
+  servFileDesc = socket(AF_INET, SOCK_STREAM, 0);
+  if (servFileDesc == -1)
   {
     perror("Socket descriptor creation failed.");
     exit(EXIT_FAILURE);
   }
+  cout << "Server file descriptor created: " << servFileDesc << endl;
 
-  if (!bind(sockFileDesc, (struct sockaddr*) &servAddress, sizeof(servAddress)))
+  /*
+  int reuse = 1;
+  if(setsockopt(servFileDesc, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) != 0);
+  {
+    perror("Socket option setting failed.");
+    exit(EXIT_FAILURE);
+  }
+  */
+  if (bind(servFileDesc, (struct sockaddr *) &servAddress, sizeof(servAddress)) == -1)
   {
     perror("Socket binding failed.");
-    close(sockFileDesc);
+    close(servFileDesc);
     exit(EXIT_FAILURE);
   }
 
-  if(!listen(sockFileDesc, MAX_BACKLOG_CONNS))
+  if(listen(servFileDesc, MAX_BACKLOG_CONNS) == -1)
   {
-    close(sockFileDesc);
+    close(servFileDesc);
     perror("Socket listening failed.");
     exit(EXIT_FAILURE);
   }
@@ -47,11 +55,11 @@ int main()
   while (stillLooping)
   {
     clientAddressLen = sizeof(clientAddress);
-    int connectFileDesc = accept(sockFileDesc,
+    int connectFileDesc = accept(servFileDesc,
       (struct sockaddr*) &clientAddress, &clientAddressLen);
 
     cout << "Connection request received..." << endl;
-    int receiveResult = recv(sockFileDesc, recMsg, MAX_LINE, 0);
+    int receiveResult = recv(connectFileDesc, recMsg, MAX_LINE, 0);
     if (receiveResult == 0)
     {
       cout << "No available messages. " << endl;
@@ -64,12 +72,11 @@ int main()
     else
     {
       cout << "Message received and resent to client: " << recMsg << endl;
-      sendMsg = recMsg;
-      send(connectFileDesc, sendMsg, strlen(sendMsg), 0);
+      send(connectFileDesc, recMsg, receiveResult, 0);
       stillLooping = false;
     }
     close(connectFileDesc);
   }
-  close(sockFileDesc);
+  close(servFileDesc);
   return EXIT_SUCCESS;
 }
