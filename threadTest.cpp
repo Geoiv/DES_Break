446d38fac53a02628ddce018744edd1632ca40a2
bjcrane <bjcrane2013@gmail.com>
2017-10-26 18:44:14 -0500

    Ready to add encrypt functionality to threads
    All Indicies and binary conversions done

--- threadTest.cpp
+++ threadTest.cpp
@@ -3,22 +3,49 @@
 #include <cmath>
 #include <pthread.h>
 #include <unistd.h>
+#include <bitset>
 
 using namespace std;
 
 #define NUM_THREADS 5
 
 struct thread_data {
-   int  thread_id;
+   int  threadId;
    long startingKeyNum;
+   long threadKeySpaceOffset;
 };
 
-void *PrintHello(void *threadarg) {
-   struct thread_data *my_data;
-   my_data = (struct thread_data *) threadarg;
+void *PrintHello(void *threadArg) {
+   struct thread_data *myData;
+   myData = (struct thread_data *) threadArg;
 
-   cout << "Thread ID : " << my_data->thread_id << endl;
-   cout << " Starting Key Number : " << my_data->startingKeyNum << endl;
+   sleep(2);
+   cout << "Thread ID : " << myData->threadId << endl;
+   sleep(1);
+   cout << " Starting Key Number : " << myData->startingKeyNum << "    " << endl;
+
+   pthread_exit(NULL);
+}
+
+void *ThreadEncrypt(void *threadArg){
+   struct thread_data *threadData;
+   threadData = (struct thread_data *) threadArg;
+
+   // Have threads explore a few more keys than they're assigned to account for
+   // mathematical errors.
+   const short buffer = 3;
+
+   unsigned long currentKey;
+   for (long i = 0; i < (threadData->threadKeySpaceOffset + buffer); i++){
+      currentKey = threadData->startingKeyNum + i;
+      cout << endl << "Thread ID:  " << threadData->threadId << "  " <<
+      " Starting Key Number : " << threadData->startingKeyNum << "    " <<
+      " Current Key Number : " << threadData->startingKeyNum + i << "    " << endl;
+      bitset<56> keyBits (currentKey);
+      cout << "keyBits: " << keyBits << endl;
+
+      // TODO call encrypt with the keyBits and the plaintext bits?
+   }
 
    pthread_exit(NULL);
 }
@@ -26,24 +53,33 @@ void *PrintHello(void *threadarg) {
 
 int main () {
    pthread_t threads[NUM_THREADS];
-   struct thread_data td[NUM_THREADS];
+   struct thread_data threadData[NUM_THREADS];
    int resultCode;
+   const long TOTAL_KEYS = pow(2, 56);
+   const short TOTAL_THREADS = 60;
+   short parentIndex = 0; // 0 - 11
 
-   long totalKeys = pow(2, 56);
-   cout << "totalKeys: " << totalKeys << endl;
+   // Client gives the index - the number of the parent
+   // The index acts as a multiplier(0-11) ---> n * (TOTAL_KEYS / 12)
+   // Use zero for first parent.
+   long parentKeySpaceStart = (TOTAL_KEYS / 12) * parentIndex;
+   cout << endl << "parentKeySpaceStart:  " << parentKeySpaceStart << endl;
 
-   for (int i = 1; i <= 12; i++) {
-      cout << (totalKeys / 12) * i << endl;
-   }
-   cout << totalKeys << endl;
-
-   int myKeyspace = totalKeys / 12;
+   // Calculates number of keys in between each threads' starting position
+   long threadKeySpaceOffset = TOTAL_KEYS/TOTAL_THREADS;
+   cout << "threadKeySpaceOffset:  " << threadKeySpaceOffset << endl;
 
    for( int i = 0; i < NUM_THREADS; i++ ) {
       cout <<"main() : creating thread, " << i << endl;
-      td[i].thread_id = i;
-      td[i].startingKeyNum = myKeyspace * i;
-      resultCode = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
+      threadData[i].threadId = i;
+      threadData[i].startingKeyNum = parentKeySpaceStart + (i * threadKeySpaceOffset);
+      threadData[i].threadKeySpaceOffset = threadKeySpaceOffset;
+
+      cout << "i: " << i << "  |  parentKeySpaceStart + (i * threadKeySpaceOffset): " <<
+      parentKeySpaceStart + (i * threadKeySpaceOffset) << endl;
+
+      // resultCode = pthread_create(&threads[i], NULL, PrintHello, (void *)&threadData[i]);
+      resultCode = pthread_create(&threads[i], NULL, ThreadEncrypt, (void *)&threadData[i]);
 
       if (resultCode) {
          cout << "Error:unable to create thread," << resultCode << endl;
